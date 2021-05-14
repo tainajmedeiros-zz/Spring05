4c273842d5072d1e5902f430be85966e178016ae
tainajmedeiros <tainajmedeiros@gmail.com>
2021-05-14 12:20:00 -0300

    core: remove imports

--- src/main/java/com/aula/aulaSpring/config/AuditingConfig.java
+++
@@ -1,9 +0,0 @@
-//package com.aula.aulaSpring.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-//
-//@Configuration
-//@EnableJpaAuditing
-//public class AuditingConfig {
-//}
--- src/main/java/com/aula/aulaSpring/config/SwaggerConfig.java
+++ src/main/java/com/aula/aulaSpring/config/SwaggerConfig.java
@@ -1,23 +1,14 @@
 package com.aula.aulaSpring.config;
 
-import com.google.common.net.HttpHeaders;
-import io.swagger.models.auth.In;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMethod;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.builders.ResponseMessageBuilder;
-import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
